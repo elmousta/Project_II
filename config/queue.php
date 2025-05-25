cef9efe5f838668788b5902baea827de7886024c
elmousta <152178409+elmousta@users.noreply.github.com>
2025-05-25 22:42:46 +0300

    Project_II

--- config/queue.php
+++ config/queue.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Queue Connection Name
+    | Default Queue Connection Names
     |--------------------------------------------------------------------------
     |
     | Laravel's queue API supports an assortment of back-ends via a single
