e9f455b5d4ddb036afeb37ab15f7dc299be860a5
elmousta <152178409+elmousta@users.noreply.github.com>
2025-05-25 22:54:22 +0300

    Project_II

--- resources/views/admin/admin-inbox.blade.php
+++ resources/views/admin/admin-inbox.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        <!-- Content Header -->
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
