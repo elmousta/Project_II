b0a2d6b9a363a787cb509c6f054ebf4162415bfd
elmousta <152178409+elmousta@users.noreply.github.com>
2025-05-25 22:53:44 +0300

    Project_II

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
+        <!-- Sidebar user panel  (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{$data->image}}" class="img-circle elevation-2" alt="User Image">
