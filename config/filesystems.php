24ae93fd77eb2084b332410ba9b546df24bc3e4d
elmousta <152178409+elmousta@users.noreply.github.com>
2025-05-25 22:40:56 +0300

    ProjectII

--- config/filesystems.php
+++ config/filesystems.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Filesystem Disk
+    | Default Filesystem Disks
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default filesystem disk that should be used
